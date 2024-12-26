336b0aa2dea2eaf4fd80121ab999102fe091919b
ayusharma-ctrl <117670536+ayusharma-ctrl@users.noreply.github.com>
2024-12-26 21:22:53 +0530

    backend: update cors config v3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
   // setting up cors policy
   app.enableCors({
-    origin: true,
+    origin: configService.get<string>('frontend_url'),
     methods: 'GET, POST, OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
